454c08aee26b9720a0e6326294b59cc69b9c720e
baiyunpeng04 <baiyunpeng04@meituan.com>
2019-08-05 14:44:07 +0800

    Init

---
+++ stock-analysis-web/src/main/java/com/sapphire/stock/web/WebStarter.java
@@ -0,0 +1,19 @@
+package com.sapphire.stock.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: 柏云鹏
+ * Date: 2019/7/29.
+ */
+
+@SpringBootApplication
+public class WebStarter {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStarter.class, args);
+    }
+
+}
---
+++ stock-analysis-web/src/main/java/com/sapphire/stock/web/page/StockController.java
@@ -0,0 +1,19 @@
+package com.sapphire.stock.web.page;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: 柏云鹏
+ * Date: 2019/7/29.
+ */
+@RestController("/stock")
+public class StockController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Test";
+    }
+}
